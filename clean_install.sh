7963568ad37ef8c10c0535eede529ec41954ac13
George Nychis <gnychis@gmail.com>
2012-05-23 16:20:35 -0400

    throw in a bootstrap and configure just incase

--- clean_install.sh
+++ clean_install.sh
@@ -2,6 +2,8 @@
 find /usr/local -name "*gnuradio*" -exec rm -fr {} \;
 echo "LIBS= -lltdl -larmadillo" >> gr-digital/lib/Makefile
 make clean
+./bootstrap
+./configure
 make -j10
 make install
 ldconfig
